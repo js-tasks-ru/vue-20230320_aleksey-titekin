7faa3620e3c9036a5ff6de1effcde78e1db4394d
Aleksey Titekin <titekin.a@yandex.ru>
2023-03-28 16:21:39 +0300

    первый шаг

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -5,5 +5,24 @@ export default defineComponent({
 
   // Компонент должен иметь входной параметр и порождать событие
 
-  template: `<button type="button">1</button>`,
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+      validator: (value) => {
+        return value >= 0
+      }
+    },
+
+    emits: ['update:count'],
+
+    method: {
+      'update:count'(value) {
+        this.$emit('update:count', value)
+      }
+    },
+
+  },
+
+  template: `<button type="button">{{ count }}</button>`,
 });
