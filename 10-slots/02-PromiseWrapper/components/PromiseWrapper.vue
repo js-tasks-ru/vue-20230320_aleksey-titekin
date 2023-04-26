240ad9993a6d93c3ecd030f9d8b0c3d866da6101
Aleksey Titekin <Titekin.A@yandex.ru>
2023-04-26 23:15:43 +0300

    10-02: Готово

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,10 +1,18 @@
 <template>
-  <!-- -->
+  <slot :name="status" :result="result" :error="error" />
 </template>
 
 <script>
 export default {
   name: 'PromiseWrapper',
+
+  data() {
+    return {
+      status: 'pending',
+      result: null,
+      error: null,
+    }
+  },
 
   props: {
     promise: {
@@ -12,5 +20,29 @@ export default {
       required: true,
     },
   },
+
+  watch: {
+    promise: {
+      deep: true,
+      immediate: true,
+      handler(newValue) {
+        this.status = 'pending';
+        newValue
+          .then(
+            (result) => {
+              this.result = result;
+              this.status = 'fulfilled';
+            }
+          )
+          .catch(
+            (error) => {
+              this.error = error;
+              this.status = 'rejected';
+            }
+          )
+      }
+    }
+  },
+
 };
 </script>
